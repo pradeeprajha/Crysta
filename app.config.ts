0b6aad9d4ae91a9d9fbdf1fa95842e63d4643cbd
pradeeprajha <57655320+pradeeprajha@users.noreply.github.com>
2025-08-15 21:11:55 +0530

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,12 @@
-// app.config.ts
-import { defineConfig } from '@tanstack/start/config'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from '@tanstack/react-start/config'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  server: { preset: 'netlify' },   // <-- important
-  vite: { plugins: [react(), tsconfigPaths()] },
+  tsr: { appDirectory: 'src' },
+  server: { preset: 'netlify' },
+  vite: {
+    plugins: [
+      viteTsConfigPaths({ projects: ['./tsconfig.json'] }),
+    ],
+  },
 })
